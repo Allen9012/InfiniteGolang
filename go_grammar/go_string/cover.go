604f5e778974d1a591b0905cf38e2ce9b7cf4f0d
zhangyiyang <zhangyiyang04@bilibili.com>
2025-06-05 17:06:02 +0800

    feat: add puml

--- go_grammar/go_string/cover.go
+++ go_grammar/go_string/cover.go
@@ -1,6 +1,11 @@
 package main
 
+import "unicode/utf8"
+
 func main() {
-	url := "https://i0.hdslb.com/bfs/tvcover/extEp_SHVRS8974932_snm_1744852145_8009b9e12c06c8455a82596091eecfda.jpg"
-	print(len(url))
+	//url := "https://i0.hdslb.com/bfs/tvcover/extEp_SHVRS8974932_snm_1744852145_8009b9e12c06c8455a82596091eecfda.jpg"
+	//print(len(url))
+
+	title := "一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十"
+	print(utf8.RuneCountInString(title))
 }
